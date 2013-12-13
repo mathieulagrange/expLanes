e57e5f7f12e540c142cbd13233c8cb6a46b43798
Lagrange <lagrange@ircam.fr>
2013-12-13 16:25:49 +0100

    second round of debugging for expCode

---
+++ expCode/expPrivate/utils/syncDependencies.m
@@ -0,0 +1,54 @@
+function syncDependencies(config, serverConfig)
+
+fprintf('Performing DEPENDENCIES sync \n');
+
+if config.localDependencies == 2
+    if serverConfig.host < 2
+        rmdir([serverConfig.codePath 'dependencies']);
+    else
+        system(['ssh ' serverConfig.hostName ' ''rm -r ' serverConfig.codePath 'dependencies 2>/dev/null ''']);
+    end
+end
+
+% TODO fix this using storage of info
+if serverConfig.host < 2
+    [status, dep] = system(['ls ' serverConfig.codePath 'dependencies']);
+else
+    [status, dep] = system(['ssh ' serverConfig.hostName ' '' ls ' serverConfig.codePath 'dependencies ''']);
+end
+if status == 0
+    dep = regexp(dep, '\n', 'split');
+else
+    dep = {};
+end
+
+for k=1:length(config.dependencies)
+    %     if ~isempty(config.dependencies{k}) && (config.dependencies{k}(1) == '\' || config.dependencies{k}(1) == '/' || config.dependencies{k}(1) == '~')
+    if any(strcmp({'/', '\'}, config.dependencies{k}(end)))
+        dependency = config.dependencies{k}(1:end-1);
+    else
+        dependency = config.dependencies{k};
+    end
+    [p n]=fileparts(dependency);
+    dep(strcmp(n, dep))=[];
+    if serverConfig.host < 2
+        syncString = 'rsync -arC --delete-after --exclude=.git ';
+        command = [syncString strrep(dependency, ' ', '\ ') ' '  serverConfig.codePath 'dependencies'];
+    else
+        syncString = 'rsync -arC   -e ssh --delete-after --exclude=.git ';
+        command = [syncString strrep(dependency, ' ', '\ ') ' ' serverConfig.hostName ':' serverConfig.codePath 'dependencies'];
+    end
+    system(command);
+    %     end
+end
+
+% remove old dependencies
+for k=1:length(dep)
+    if ~isempty(dep{k})
+        if serverConfig.host < 2
+            rmdir([serverConfig.codePath 'dependencies' filesep dep{k}]);
+        else
+            system(['ssh ' serverConfig.hostName ' ''rm -r ' serverConfig.codePath 'dependencies' filesep dep{k} ' 2>/dev/null ''']);
+        end
+    end
+end
---
+++ expCode/expPrivate/utils/syncDirectory.m
@@ -0,0 +1,151 @@
+function syncDirectory(config, serverConfig, directoryName, deleteOld, detach, extensionPath, selector)
+
+if nargin<4, deleteOld=0; end
+if nargin<5, detach=0; end
+if nargin<6, extensionPath = ''; end
+if nargin<7, selector = []; end
+
+if any(strcmp(config.taskName, directoryName))
+    directoryPath = [config.dataPath directoryName filesep extensionPath];
+    serverDirectoryPath = [serverConfig.dataPath directoryName filesep extensionPath];
+else
+    directoryPath = [eval(['config.' directoryName 'Path'])  extensionPath];
+    serverDirectoryPath = [eval(['serverConfig.' directoryName 'Path'])  extensionPath];
+end
+
+syncString = 'rsync -arC    --exclude=.git --update ';
+deleteString = [' --delete-after --backup --backup-dir=' config.backupPath ' '];
+if detach
+    detachString = ' >/dev/null &';
+    detachMessage = 'in silent and detached mode';
+    verboseString = '';
+else
+    detachString = '';
+    detachMessage = '';
+    verboseString = '-v ';
+end
+if deleteOld
+    syncString = [syncString deleteString];
+    disp(['Deletion is selected. Backed up data is available at ', config.backupPath])
+end
+
+
+% a: archive mode
+% C: exclude cvs like files
+% update: skip newer files
+
+% TODO: be able to control update
+% TODO: use the filesep command
+
+fprintf('Performing %s %s sync of project %s %s ', upper(directoryName), extensionPath, config.projectName, detachMessage);
+
+% create it if needed
+if serverConfig.host < 2
+    if ~exist(serverDirectoryPath, 'dir')
+        mkdir(serverDirectoryPath);
+    end
+else
+    syncString = [syncString  ' -e ssh '];
+    if system(['ssh ' serverConfig.hostName ' '' ls ' serverDirectoryPath ' 2>/dev/null ''']) % TODO remove output when it fails put & ?
+        system(['ssh ' serverConfig.hostName ' ''mkdir -p ' serverDirectoryPath '''']);
+    end
+end
+
+switch lower(config.syncDirection(1))
+    % TODO special case of local
+    case 'u'
+        fprintf('from host to server %s\n', serverConfig.hostName);
+        ori = directoryPath;
+         if serverConfig.host < 2
+            dest = serverDirectoryPath;
+        else
+            dest = [serverConfig.hostName ':' serverDirectoryPath];
+        end
+%         dest = [serverConfig.hostName ':' serverDirectoryPath];
+        excludeString=getExcludeString(config, directoryPath);
+    case 'd'
+        fprintf('from server %s to host\n', serverConfig.hostName);
+        dest = directoryPath;
+        excludeString=getExcludeString(serverConfig, serverDirectoryPath);
+        
+         if serverConfig.host < 2
+            ori = serverDirectoryPath;
+        else
+            ori = [serverConfig.hostName ':' serverDirectoryPath];
+        end
+    case 'c'
+        fprintf('cleaning host %s\n', serverConfig.hostName);
+        if config.syncDirection(1)=='c'
+            fprintf('Backed up data is available at %s\n', config.backupPath);
+        end
+        excludeString=getExcludeString(serverConfig, serverDirectoryPath);
+        if ~exist(config.backupPath, 'file')
+            mkdir(config.backupPath);
+        end
+        dest = config.backupPath;
+        if serverConfig.host < 2
+            ori = serverDirectoryPath;
+        else
+            ori = [serverConfig.hostName ':' serverDirectoryPath];
+        end
+        
+end
+
+selectorString = '';
+if ~isempty(selector)
+    switch selector
+        case 's'
+            selectorString = '_store.mat';
+            excludeString = [excludeString '--include ''*_store.mat'' --exclude ''*'''];
+        case 'd'
+            selectorString = '_display.mat';
+            excludeString = [excludeString '--include ''*_display.mat'' --exclude ''*'''];
+    end
+end
+
+commandString = [syncString verboseString excludeString ' '  ori '/ ' dest detachString];
+% commandString
+
+if config.syncDirection(1)~='C'
+    system(commandString);
+end
+
+if lower(config.syncDirection(1))=='c'
+    if serverConfig.host < 2
+        removeCommand = ['find ' serverDirectoryPath ' -name "*' selectorString '" -maxdepth 1 -print0 | xargs -0 rm -f 2>/dev/null '];
+    else
+        removeCommand = ['ssh ' serverConfig.hostName ' ''find ' serverDirectoryPath ' -name "*" -maxdepth 1 -print0 | xargs -0 rm -f' ''' 2>/dev/null '];
+    end
+    
+    system(removeCommand);
+end
+
+% remove reduceData
+reduceDataFileName = [directoryPath 'reduceData.mat'];
+if exist(reduceDataFileName, 'file')
+    delete(reduceDataFileName);
+end
+
+function excludeString=getExcludeString(config, directoryPath)
+
+
+% excludeString = ' --filter=''- dependencies''';
+excludeString ='';
+fieldNames=config.taskName;
+for k=1:length(fieldNames)
+    pathName = [config.dataPath fieldNames{k} filesep];
+    index = strfind(pathName, directoryPath);
+    if  ~strcmp(pathName, directoryPath) && ~isempty(index)
+        excludeString = [excludeString ' --filter=''- ' pathName(length(directoryPath):end)  ''''];
+    end
+end
+
+
+fieldNames={'code' 'report' 'input' 'bundle'};
+for k=1:length(fieldNames)
+    pathName = eval(['config.' fieldNames{k} 'Path']);
+    index = strfind(pathName, directoryPath);
+    if  ~strcmp(pathName, directoryPath) && ~isempty(index)
+        excludeString = [excludeString ' --filter=''- ' pathName(length(directoryPath):end)  ''''];
+    end
+end
