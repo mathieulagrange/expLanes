26bf13a744399e1db8ea1b36ce232eda6a8152da
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2014-09-12 12:11:20 +0200

    fix ranking and add image plot

---
+++ expCode/nonExposed/expose/exposeImage.m
@@ -0,0 +1,22 @@
+function config = exposeImage(config, data, p)
+
+config = expDisplay(config, p);
+
+if p.sort
+    [data.meanData, ind] = expSortData(data.meanData, p, 0, config);
+    labels = flipud(p.rowNames(ind));
+else
+    labels = flipud(p.rowNames);
+end
+
+imagesc(flipud(data.meanData));
+
+axis xy
+p.orientation='v';
+p.labels = p.legendNames;
+p.legend=0;
+expSetAxes(config, p);
+
+set(gca, 'yticklabel', labels);
+    colorbar
+
