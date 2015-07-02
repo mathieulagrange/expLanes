a27b79d3ebe85e95a7fb3ff1b0a467c6ac966018
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2015-07-02 14:55:54 +0200

    fixme

--- expCode/nonExposed/expConfig.m
+++ expCode/nonExposed/expConfig.m
@@ -56,6 +56,8 @@ function config = expConfig(experimentPath, experimentName, shortExperimentName,
 
 % TODO sorting for all exposition
 
+% FIXME check if codePath is wrong
+
 expCodePath = fileparts(fileparts(mfilename('fullpath')));
 
 if exist('commands', 'var') && ~isempty(commands) && isstruct(commands{1})
