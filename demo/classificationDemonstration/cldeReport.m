eed8b804d78a51e21a00b89f360cebdd2f163a34
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2015-11-09 11:56:59 +0100

    first version of classification demo

--- demo/classificationDemonstration/cldeReport.m
+++
@@ -1,5 +0,0 @@
-function config = cldeReport(config)
-
-if nargin==0, classificationDemo('report', -1); return; end
-
-% config = expExpose(config, 'confusionMatrix', 'mask', {1 1 1 1 1 1:2}, 'put', 0);
