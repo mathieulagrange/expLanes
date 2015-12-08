166073a496601257fd2c01aae6ce67e0fdf668ba
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2015-12-08 10:51:15 +0100

    addition of factors png files

--- demo/syncReports.m
+++ demo/syncReports.m
@@ -4,4 +4,5 @@ r=dir('*pdf');
 for k=1:length(r)
    [~, n] = fileparts(r(k).name);
    copyfile([n '/report/' n '.pdf'], '.'); 
+   system(['convert -density 300 ' n '/report/figures/factors.pdf -quality 90 ' n '/report/figures/factors.png']);
 end
