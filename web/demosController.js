888a2cf1447c815af011ae3fd52afc9371f353ea
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2016-02-02 10:16:46 +0100

    final touch

--- web/demosController.js
+++ web/demosController.js
@@ -1,10 +1,10 @@
 app.controller("DemosController", function($scope) {
 
     $scope.demos = [
-	{title: "Comparison of distances", html:"demo1.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/distanceComputation", pdf:"demo/distanceComputation/report/distanceComputation.pdf"},
-	{title: "Clustering", html:"demo2.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/clusteringData", pdf:"demo/clusteringData/report/clusteringData.pdf"},
-	{title: "Classification", html:"demo3.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/classifyData", pdf:"demo/classifyData/report/classifyData.pdf"},
-	{title: "Audio source separation", html:"demo4.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/denoiseAudio", pdf:"demo/denoiseAudio/report/denoiseAudio.pdf"}
+	{title: "Comparison of distances", html:"demo1.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/distanceComputation", pdf:"demo/distanceComputation.pdf"},
+	{title: "Clustering", html:"demo2.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/clusteringData", pdf:"demo/clusteringData.pdf"},
+	{title: "Classification", html:"demo3.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/classifyData", pdf:"demo/classifyData.pdf"},
+	{title: "Audio source separation", html:"demo4.html", url:"https://github.com/mathieulagrange/expLanes/blob/master/demo/denoiseAudio", pdf:"demo/denoiseAudio.pdf"}
     ];
 
 //    $scope.demos.forEach(function(d) {
